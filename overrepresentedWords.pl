f93e209e76c192bd2f24f720ac4982d60bb86281
KevinBretonnelCohen <kevin.cohen@gmail.com>
2020-03-26 18:46:50 -0700

    Archival commit--don't use yet.

--- overrepresentedWords.pl
+++ overrepresentedWords.pl
@@ -7,13 +7,13 @@ my %corpus02 = ();
 
 my $smoothing_factor = 100; # Adam liked 100--don't remember why
 
-open(IN1, "/Users/transfer/Dropbox/Scripts-new/cord19.lexical.frequencies.txt") || die "$!\n";
-open(IN2, "/Users/transfer/Dropbox/Scripts-new/craft.lexical.frequencies.txt") || die "$!\n";
+#open(IN1, "/Users/transfer/Dropbox/Scripts-new/cord19.lexical.frequencies.txt") || die "$!\n";
+#open(IN2, "/Users/transfer/Dropbox/Scripts-new/craft.lexical.frequencies.txt") || die "$!\n";
 #open(IN2, "/Users/transfer/Dropbox/Scripts-new/craft.10.words.txt") || die "$!\n";
 
 # test case: all ratios should be 1.0, because you're comparing a corpus against itself
-#open(IN1, "/Users/transfer/Dropbox/Scripts-new/craft.word.frequencies.txt") || die "$!\n";
-#open(IN2, "/Users/transfer/Dropbox/Scripts-new/craft.word.frequencies.txt") || die "$!\n";
+open(IN1, "/Users/transfer/Dropbox/Scripts-new/craft.lexical.frequencies.txt") || die "$!\n";
+open(IN2, "/Users/transfer/Dropbox/Scripts-new/craft.lexical.frequencies.txt") || die "$!\n";
 
 # test case: completely disjunct vocabularies, so big numbers for A-E and small numbers for V-Z
 #open(IN1, "/Users/transfer/Dropbox/Scripts-new/test.overrepresented.01.txt") || die "$!\n";
@@ -41,6 +41,7 @@ while (my $line = <IN2>) {
 }
 close(IN2);
 
+# count the sizes of the two corpora so that you can go from counts to frequencies
 my $corpus01_size = 0;
 foreach my $word (keys(%corpus01)) {
     0 && print "$word: frequency = $corpus01{$word}\n";
@@ -75,37 +76,61 @@ my $unioned_words_vocabulary_size = @unioned_words;
 
 my %ratios = ();
 
+# DETERMINE THE RAW FREQUENCIES (i.e. smoothed counts)
+# smooth the raw frequencies for both corpora to avoid divide-by-zero errors when you later calculate the relative frequencies.
+
+my %corpus01_raw_frequencies;
+my %corpus02_raw_frequencies;
+
+my $c1_raw_freq;
+my $c2_raw_freq;
+
 foreach my $word (@unioned_words) {
 
-    my $c1_raw_freq;
+    # the corpus of interest
     if ($corpus01{$word}) {
        $c1_raw_freq = $corpus01{$word} + $smoothing_factor;
     } else {
 	$c1_raw_freq = $smoothing_factor;
     }
-    my $c2_raw_freq;
+
+    $corpus01_raw_frequencies{$word} = $c1_raw_freq;
+
+    # the reference corpus
     if ($corpus02{$word}) {
 	$c2_raw_freq = $corpus02{$word} + $smoothing_factor;
     } else {
 	$c2_raw_freq = $smoothing_factor;
     }
 
+    $corpus02_raw_frequencies{$word} = $c2_raw_freq;
+
+} # close foreach-loop to determine raw frequencies
+
+    # CALCULATE THE RELATIVE FREQUENCIES
     # TODO: do the corpus sizes need to be adjusted for the smoothing that we did??? XXX
 
-    # due to the smoothing that we did, there should never be a zero in either the denominator or the numerator
-    my $c1_relative_freq = $c1_raw_freq / $corpus01_size;
-    my $c2_relative_freq = $c2_raw_freq / $corpus02_size;
+    # due to the smoothing that we do, there should never be a zero in either the denominator or the numerator
 
-    0 && print "<$word> C1 relative: $c1_relative_freq C2 relative: $c2_relative_freq\n";
+    # YOU ARE HERE. Take a break and then note that you need to have stored the relative frequencies up above.  I think that you need to adjust the corpus sizes for the smoothing, too.
+    # note that if you only do this for corpus01, then you only have the relative frequencies for words in corpus01, and *don't* have them for words that are in corpus01 but *not* in corpus02.  TODO: verify that that's what you should be doing.  I guess that to get them for the other corpus, you just reverse the file handle names (IN1 and IN2) when you first read them in.
+    foreach my $word (@corpus01_words) {
+      my $c1_relative_freq = $c1_raw_freq / $corpus01_size;
+      my $c2_relative_freq = $c2_raw_freq / $corpus02_size;
 
-    my $ratio = $c1_relative_freq / $c2_relative_freq;
-    0 && print "<$word> ratio: $ratio\n";
-    $ratios{$word} = $ratio;
-} # close foreach-loop through set of all words in both corpora
+      0 && print "<$word> C1 relative: $c1_relative_freq C2 relative: $c2_relative_freq\n";
 
+      my $ratio = $c1_relative_freq / $c2_relative_freq;
+      0 && print "<$word> ratio: $ratio\n";
+
+      $ratios{$word} = $ratio;
+
+    } # close foreach-loop to calculate relative frequencies
+ 
 #my @ordered_by_ratio = sort { $b <=> $a } keys(%ratios);
 my @ordered_by_ratio = sort { $ratios{$b} <=> $ratios{$a} } keys(%ratios);
 
 foreach my $word (@ordered_by_ratio) {
     print "$word,$ratios{$word}\n";
 }
+
